4cf2397346fc90c688d2785ee23dd8e3376907b9
RRouxx <pks_atlas@hotmail.com>
2024-02-05 21:34:47 -0600

    Movement & Cinemachine
    Se agregó el movimiento WASD y Jump. Se estableció la cámara en tercera persona

---
+++ Titulacion Game/Assets/Scripts/SwitchVCam.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using Cinemachine;
+
+public class SwitchVCam : MonoBehaviour
+{
+    [SerializeField]
+    private PlayerInput playerInput;
+
+    private CinemachineVirtualCamera virtualCamera;
+    private InputAction aimAction;
+
+    private void Awake()
+    {
+        
+    }
+
+    private void OnEnabled()
+    {
+
+    }
+
+    private void OnDisabled()
+    {
+
+    }
+
+    private void StartAim()
+    {
+
+    }
+
+    private void CancelAim()
+    {
+
+    }
+}
